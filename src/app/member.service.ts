9b7657a401b09db8c9b15c60b62a2a2af10984a5
dishasachdeva <127543878+DishaSachdeva-02@users.noreply.github.com>
2024-04-10 16:40:27 +0530

    update

--- src/app/member.service.ts
+++ src/app/member.service.ts
@@ -4,6 +4,7 @@ import { MEMBERS } from './My-members';
 import { Observable,of } from 'rxjs';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { tap,catchError,map } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -13,22 +14,56 @@ export class MemberService {
   constructor(private messageService:MessageService , private http:HttpClient) { }
   getMember():Observable<Member[]>{
     
-     this.messageService.add("Members are displayed....")
-     return this.http.get<Member[]>(this.membersurl);
+    //  this.messageService.add("Members are displayed....")
+     return this.http.get<Member[]>(this.membersurl).pipe(
+      tap(_=>this.log('fetched members..')),
+     catchError(this.handleError<Member[]>('getMember',[]))
+    );;
   }
   getMem(id:number):Observable<Member>{
     const url=`${this.membersurl}/${id}`;
     this.messageService.add(`member displayed is ${id}`);
-    return this.http.get<Member>(url);
+    return this.http.get<Member>(url).pipe(
+      tap(_=>this.log(`fetched member detail whose id=${id}...`)),
+      catchError(this.handleError<Member>(`getMem id=${id}`))
+    );
   }
   updateMember(member: Member): Observable<any> {
-    return this.http.put(this.membersurl, member, this.httpOptions);
+    return this.http.put(this.membersurl, member, this.httpOptions).pipe(
+      tap(_=>this.log(`updated member whose id=${member.id}`)),
+      catchError(this.handleError<any>('updateMember'))
+    );;
   }
   addMember(member:Member):Observable<Member>{
-    return this.http.post<Member>(this.membersurl,member,this.httpOptions);
+    return this.http.post<Member>(this.membersurl,member,this.httpOptions).pipe(
+      tap((newmember:Member)=>this.log(`New member added with id=${newmember.id}`)),
+      catchError(this.handleError<Member>('addchar'))
+    );
   }
   deleteMember(id:number):Observable<Member>{
     const url=`${this.membersurl}/${id}`;
-    return this.http.delete<Member>(url,this.httpOptions);
+    return this.http.delete<Member>(url,this.httpOptions).pipe(
+      tap(_=>this.log(`deleted member whose id=${id}`)),
+      catchError(this.handleError<Member>('deleteMember'))
+    );
   }
+  search(term:string):Observable<Member[]>{
+    if(!term.trim()){
+      return of([]);
+    }
+    return this.http.get<Member[]>(`${this.membersurl}/?name=${term}`).pipe(
+      tap(_=>this.log(`searching data for ${term}`)),
+      catchError(this.handleError<any>('searchdata',[]))
+    );
+  }
+  private handleError<T>(operation='operation', result?:T){
+    return (error:any):Observable<T>=>{
+     this.log(`${operation} failed: ${error.message}`)
+     console.log(error);
+      return of(result as T)
+    }
+ }
+ private log(message:string){
+   this.messageService.add(`${message}`);
+ }
 }
